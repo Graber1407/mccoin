2ac2ece7940756b5d5881a6206916d029ff2e2cb
Abdullaev Alisultan <sultan7306@mail.ru>
2022-04-24 14:30:09 +0300

    Test blockchain

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	bc "github.com/Graber1407/mccoin/blockchain"
+)
 
+func main() {
+	chain := bc.InitBlockChain()
+
+	chain.AddBlock("John sent 1 mccoin to Alice")
+	chain.PrintChain()
 }
