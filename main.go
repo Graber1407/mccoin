67074cb18a2b4fe286db20c74cd51bbcc3a5b312
Abdullaev Alisultan <sultan7306@mail.ru>
2022-04-24 13:55:20 +0300

    Initial commmit

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+)
+
+type BlockChain struct {
+	blocks []*Block
+}
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
+
+func (chain *BlockChain) PrintChain() {
+	for index, block := range chain.blocks {
+		fmt.Printf("-------------------%d-----------------\n", index)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("PrevHash: %x\n", block.PrevHash)
+		fmt.Println("-------------------------------------\n\n")
+	}
+}
+
+func main() {
+	chain := InitBlockChain()
+
+	chain.AddBlock("0x783acZGuYT80xYlkfF send 1 mccoin to 0x3183acGuMg80xYlkfL")
+	chain.PrintChain()
+}
